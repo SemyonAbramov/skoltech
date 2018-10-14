779bc34d6b627723479169ffab99f45a143396f6
Semyon Abramov <sabramov@Semyons-MacBook-Pro.local>
2018-10-14 16:45:24 +0300

    [MAJOR] Added simple queue and bfs algorithm

--- EADS/main.c
+++ EADS/main.c
@@ -79,32 +79,121 @@ void print_adj_lists()
 		}
 	}
 }
+
 /*
-void bfs(node_t** adj_list, int src)
+Queue impementation
+*/
+
+typedef struct queue
+{ 
+    int front, rear, size; 
+    unsigned capacity; 
+    int* array; 
+}queue_t; 
+  
+queue_t* createQueue(unsigned capacity) 
+{ 
+    queue_t* queue = (queue_t*) malloc(sizeof(queue_t)); 
+    queue->capacity = capacity; 
+    queue->front = queue->size = 0;  
+    queue->rear = capacity - 1;  // This is important, see the enqueue 
+    queue->array = (int*) malloc(queue->capacity * sizeof(int)); 
+    return queue; 
+} 
+  
+// Queue is full when size becomes equal to the capacity  
+int isFull(queue_t* queue) 
+{  return (queue->size == queue->capacity);  } 
+  
+// Queue is empty when size is 0 
+int isEmpty(queue_t* queue) 
+{  return (queue->size == 0); } 
+  
+// Function to add an item to the queue.   
+// It changes rear and size 
+void enqueue(queue_t* queue, int item) 
+{ 
+    if (isFull(queue)) 
+        return;
+    queue->rear = (queue->rear + 1)%queue->capacity; 
+    queue->array[queue->rear] = item; 
+    queue->size = queue->size + 1; 
+    printf("%d enqueued to queue\n", item); 
+} 
+  
+// Function to remove an item from queue.  
+// It changes front and size 
+int dequeue(queue_t* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return -1; 
+    int item = queue->array[queue->front]; 
+    queue->front = (queue->front + 1)%queue->capacity; 
+    queue->size = queue->size - 1; 
+    return item;
+} 
+  
+// Function to get front of queue 
+int front(queue_t* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return -1; 
+    return queue->array[queue->front]; 
+} 
+  
+// Function to get rear of queue 
+int rear(queue_t* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return -1; 
+    return queue->array[queue->rear]; 
+} 
+
+////////////////////////////////////////
+
+void bfs(vertice_t* adj_list, int src)
 {
-	node_t* list;
-	int i;
+	successor_node_t* list;
 	int visited[size_of_adj_lists];
 
+	int i;
 	for (i = 0; i < size_of_adj_lists; i++) {
 		visited[i] = 0;
 	}
 
+	adj_list[src].unw_shortest = 0;
 
-	for (i = 0; i < size_of_adj_lists; i++) {
-		list = adj_list[i];
+	queue_t* queue = createQueue(size_of_adj_lists);
+	enqueue(queue, src);
+
+	int dq = 0;
+
+	while (isEmpty(queue) != 1) {
+		dq = dequeue(queue);
+
+		list = adj_list[dq].next_adj_node;
 
 		while (list) {
-			
-			list->unw_path_len = -1;
+			if (!visited[list->dst]) {
+				visited[list->dst] = 1;
+				adj_list[list->dst].unw_shortest = adj_list[dq].unw_shortest + 1;
+				enqueue(queue, list->dst);
+			}
 			list = list->next;
 		}
 	}
-
-
-
 }
-*/
+
+
+void print_unw_shortest(vertice_t* adj_list, int s)
+{
+	int i;
+	for (i = 0; i < size_of_adj_lists; i++) {
+		printf("shortest path from %d to %d: %d\n", s, i, adj_list[i].unw_shortest);
+	}
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -161,6 +250,10 @@ int main(int argc, char** argv)
 
 	printf("### print created lists ###\n");
 	print_adj_lists();
+
+	bfs(adj_lists, source_node_index);
+
+	print_unw_shortest(adj_lists, source_node_index);
 
 	return 0;
 }
